b98415c2cb6c3ba7faaf2e9135504d14f52f6383
meliscentaquino <meliscenjhoy.aquino@lorma.edu>
2026-01-21 04:02:40 +0800

    @nd Week activity

--- week2/Week2activity1.java
+++ week2/Week2activity1.java
@@ -6,10 +6,11 @@ public class Week2activity1 {
         theaterRow[3] = 1;
 
         for (int i = 0; i < theaterRow.length; i++) {
+            
             if (theaterRow[i] == 0){
-                System.out.printf("Seat %4wee: Available", theaterRow[i]);
+                System.out.printf("Seat %d: Available\n", i+1);
             } else {
-                System.out.printf("Seat %4: Booked", theaterRow[i]);
+                System.out.printf("Seat %d: Booked\n", i+1);
             }             
         }System.out.println();   
     }
