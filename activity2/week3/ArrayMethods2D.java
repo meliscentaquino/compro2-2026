868e977b2f11a229b8393bdbe89f406f13ac0e3a
meliscentaquino <meliscentjhoy.aquino@lorma.edu>
2026-01-27 12:17:53 +0800

    3rd Week Activity2

--- activity2/week3/ArrayMethods2D.java
+++ activity2/week3/ArrayMethods2D.java
@@ -3,22 +3,36 @@ import java.util.*;
 public class ArrayMethods2D {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        //prob 1
         int rows = 3;
         int column = 4;
-        double[][] numbers = new double [rows][column];
+        double[][] table1 = new double [rows][column];
 
         System.out.println("Enter a 3-by-4 matrix row by row:");
         for (int r = 0; r < rows; r++) {
                 for (int c = 0; c < column; c++) {
-                    numbers[r][c] = input.nextDouble();
+                    table1[r][c] = input.nextDouble();
                 }
             }
         
         for (int c = 0; c < column; c++) {
-            System.out.println("Sum of the element at column " + c + " is " + sumColumn(numbers, c));
+            System.out.println("Sum of the element at column " + c + " is " + sumColumn(table1, c));
         }
-    }
+        
+        //prob 2
+        int rcSize = 4;
+        double[][] table2 = new double [rcSize][rcSize];
 
+        System.out.println("Enter a 4-by-4 matrix row by row:");
+        for (int row = 0; row < rcSize; row++) {
+                for (int col = 0; col < rcSize; col++) {
+                    table1[row][col] = input.nextDouble();
+                }
+            }
+        System.out.println("Sum of the element major diagonal is " + sumDiagonal(table2));
+
+    }
+        //prob 1
         public static double sumColumn (double[][] numbers, int column){
             double sum = 0;
             for (int row = 0; row < numbers.length; row++) {
@@ -26,7 +40,12 @@ public class ArrayMethods2D {
             }
             return sum;
         }
+        //prob 2
         public static double sumDiagonal(double[][] numbers) {
-
+            double sum = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                sum += numbers[i][i];
+            }
+            return sum;
         }
 }
